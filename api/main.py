f43e05d65dd94b5ccbf4e5633e171db71346d6dc
Otter J <114224792+lhmqk@users.noreply.github.com>
2023-04-09 23:11:36 +0700

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import random
+import time
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-	return "HELLO from vercel use flask"
+@app.route('/')
+def get_random_number():
+    return str(random.randint(1, 100))
+
+if __name__ == '__main__':
+    while True:
+        app.run(debug=True)
+        time.sleep(60)
