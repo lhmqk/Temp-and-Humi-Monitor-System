b4a2509be15e5b1907d918c41748e6f5c4f0865e
Otter J <114224792+lhmqk@users.noreply.github.com>
2023-04-09 23:09:48 +0700

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,17 +1,6 @@
-from flask import Flask,request
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 	return "HELLO from vercel use flask"
-
-@app.route('/example', methods=['POST'])
-def example():
-    if request.method == 'POST':
-        data = request.form['data']
-        print(data)
-        return f"The data you sent is: {data}"
-
-@app.route("/about")
-def about():
-	return "HELLO about"
