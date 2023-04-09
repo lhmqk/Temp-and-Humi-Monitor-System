c6f37e67b0a980da43947b7c1a4abad5f795625d
Otter J <114224792+lhmqk@users.noreply.github.com>
2023-04-09 23:08:22 +0700

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,9 +1,17 @@
-import random
-import time
-from flask import Flask
-
+from flask import Flask,request
 app = Flask(__name__)
 
-@app.route('/')
-def get_random_number():
-    return str(random.randint(1, 100))
+@app.route("/")
+def home():
+	return "HELLO from vercel use flask"
+
+@app.route('/example', methods=['POST'])
+def example():
+    if request.method == 'POST':
+        data = request.form['data']
+        print(data)
+        return f"The data you sent is: {data}"
+
+@app.route("/about")
+def about():
+	return "HELLO about"
